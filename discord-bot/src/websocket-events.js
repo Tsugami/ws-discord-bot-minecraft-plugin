3b050b90f40a2849d5fe638d1c84b5fdb413d286
Yslan Ramos <yslan3000@gmail.com>
2021-04-10 03:26:34 -0300

    fix onReady

--- discord-bot/src/websocket-events.js
+++ discord-bot/src/websocket-events.js
@@ -1,10 +1,22 @@
-module.exports = (io) => {
+
+
+module.exports = (io, discordBot) => {
+  const onMessage = ({ username, message, createdAt }) => {
+    if (!username || !message || !createdAt) {
+      return;
+    }
+
+    const content = `[${createdAt}] ${username} - ${message}`;
+    discordBot.sendMessage(content);
+  }
+
   io.on('connection', (socket) => {
     console.info(`[INFO] (WS) ${socket.id} connected.`);
 
     socket.on('message', (payload) => {
       console.info(`[INFO] (WS) ${socket.id} send message received!`);
       console.info(`[INFO] (WS) ${socket.id} payload `, payload);
+      onMessage(payload);
     });
   });
 }
