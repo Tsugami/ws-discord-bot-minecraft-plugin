68e5063463e7552a09eb68a433cb41926bd57767
Yslan Ramos <yslan3000@gmail.com>
2021-04-10 03:20:24 -0300

    feat(DiscordBot): DIP

--- discord-bot/src/discord-bot.js
+++ discord-bot/src/discord-bot.js
@@ -1,14 +1,33 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`[INFO] (DISCORD) Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+const { Client } = require('eris');
+class DiscordBot {
+  constructor (discordBotToken, discordChannelID) {
+    this.discordChannelID = discordChannelID;
+    this._bot = new Client(discordBotToken);
   }
-});
 
-module.exports = client;
+  createMessage (content) {
+    return this._bot.createMessage(this.discordChannelID, content);
+  }
+
+  connect () {
+    this._registerEvents();
+    return this._bot.connect();
+  }
+
+  _registerEvents () {
+    this._bot.on('ready', this._onReady.bind(this));
+    this._bot.on('messageCreate', this._onMessage.bind(this));
+  }
+
+  _onReady () {
+    console.log(`[INFO] (DISCORD) Logged in as ${client.user.tag}!`);
+  }
+
+  _onMessage (msg) {
+    if (msg.content === 'ping') {
+      this._bot.createMessage(msg.channel.id, "Pong!");
+    }
+  }
+}
+module.exports = DiscordBot;
--- discord-bot/src/index.js
+++ discord-bot/src/index.js
@@ -3,19 +3,19 @@ require('dotenv/config');
 const express = require('express');
 const { Server } = require("socket.io");
 const http = require('http');
+const DiscordBot = require('./discord-bot');
 const websocketEvents = require('./websocket-events');
-const client = require('./discord-bot');
 
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
+const bot = new DiscordBot(process.env.DISCORD_TOKEN, process.env.DISCORD_CHANNEL_ID);
 
-websocketEvents(io);
+websocketEvents(io, bot);
 
 const PORT = process.env.PORT || 3333;
-const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 httpServer.listen(PORT, () => {
-  console.info(`[INFO] (SERVER) http server listening at http://localhost:${PORT}`)
-  client.login(DISCORD_TOKEN)
+  console.info(`[INFO] (SERVER) http server listening at http://localhost:${PORT}`);
+  bot.connect();
 })
