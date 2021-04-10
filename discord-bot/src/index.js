e6576fff70e589e1bd6001c7d75da2cb3d4f8668
Yslan Ramos <yslan3000@gmail.com>
2021-04-10 02:26:13 -0300

    add express

--- discord-bot/src/index.js
+++ discord-bot/src/index.js
@@ -1,9 +1,11 @@
+const express = require('express');
 const { Server } = require("socket.io");
 const http = require('http');
 const websocketEvents = require('./websocket-events');
 
 
-const httpServer = http.createServer();
+const app = express();
+const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
 websocketEvents(io);
