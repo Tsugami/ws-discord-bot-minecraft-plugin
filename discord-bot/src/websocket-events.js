5290802d9c439c9812e76029d55baa077f28b767
Yslan Ramos <yslan3000@gmail.com>
2021-04-10 02:54:58 -0300

    fix loggers

--- discord-bot/src/websocket-events.js
+++ discord-bot/src/websocket-events.js
@@ -1,10 +1,10 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.info(`[INFO] (${socket.id}) new connected.`);
+    console.info(`[INFO] (WS) ${socket.id} connected.`);
 
     socket.on('message', (payload) => {
-      console.info(`[INFO] (${socket.id}) new message received!`);
-      console.info(`[INFO] (${socket.id}) payload ${payload}.`);
+      console.info(`[INFO] (WS) ${socket.id} send message received!`);
+      console.info(`[INFO] (WS) ${socket.id} payload `, payload);
     });
   });
 }
